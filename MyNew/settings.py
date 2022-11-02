9a70b358a7b5f627eb8d0318e910a2a3f588d2ad
Quoc Huy <quochuyrocket@gmail.com>
2022-11-02 19:11:02 +0700

    deploy version 1

--- MyNew/settings.py
+++ MyNew/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-a&v)q3uw5rz#$y4blp4amq&w6p&6dye7pti6ah$#a5p3@^hei(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["pythonstories.herokuapp.com", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ['https://pythonstories.herokuapp.com']
 
 
 # Application definition
