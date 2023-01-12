7242c4877c08d1ca76c4ed223d931a06cfbb10c5
quochuybh0404 <70765405+quochuybh0404@users.noreply.github.com>
2023-01-12 16:55:46 +0700

    Update settings.py

--- MyNew/settings.py
+++ MyNew/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a&v)q3uw5rz#$y4blp4amq&w6p&6dye7pti6ah$#a5p3@^hei(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stories-project.vercel.app']
 
 
 # Application definition
