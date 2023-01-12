1caf0c0b0d579d6e5f26b84779a74a8d6a7eee96
quochuybh0404 <70765405+quochuybh0404@users.noreply.github.com>
2023-01-12 17:21:30 +0700

    Update settings.py

--- MyNew/settings.py
+++ MyNew/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'MyNew.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
+DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-# }
+}
 
 DATABASES = {
     'default': {
